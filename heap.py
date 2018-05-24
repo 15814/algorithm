41072baf3ca5dded212738c60bdfaf933791c6bd
15814 <acat99@foxmail.com>
2018-05-24 11:50:50 +0800

    add heap_pop() and test_heap_pop()

--- heap.py
+++ heap.py
@@ -38,22 +38,67 @@ def printheap(elements: list):
     return
 
 
+def heap_pop(elements: list):
+    if not elements:
+        return
 
-def test_heap():
+    elements[0],elements[len(elements)-1] = elements[len(elements)-1],elements[0]
+
+    elem = elements.pop(len(elements)-1)
+    if elements:
+        heapify(elements)
+
+    return elem
+
+
+def heapify(elems: list):
+    if not elems or len(elems) == 1:
+        return
+    if len(elems) == 2:
+        if elems[0] < elems[1]:
+            elems[0], elems[1] = elems[1], elems[0]
+
+    if len(elems) > 2:
+        cur = 0
+        lchild = 2 * cur + 1
+        rchild = lchild + 1
+        while elems[cur] < elems[lchild] or elems[cur] < elems[rchild]:
+            max = (lchild if elems[lchild] > elems[rchild] else rchild)
+            elems[cur], elems[max] = elems[max], elems[cur]
+            cur = max
+            lchild = 2 * cur + 1
+            rchild = lchild + 1
+            if lchild >= len(elems):
+                break
+            if rchild >= len(elems):
+                rchild = lchild
+
+        return
+
+
+
+
+
+def test_heapinsert(elems: list):
     import random
-    elements = []
     for i in range(10):
         elem = random.randrange(1,100)
-        heapinsert(elements,elem)
-    printheap(elements)
+        heapinsert(elems,elem)
+    printheap(elems)
 
+def test_heap_pop(elems: list):
+    while elems:
+        print(heap_pop(elems),end='    ')
+
+    return
 
 
 
 
 def main():
-    test_heap()
-
+    elems =[]
+    test_heapinsert(elems)
+    test_heap_pop(elems)
 
 if __name__ == '__main__':
     main()
