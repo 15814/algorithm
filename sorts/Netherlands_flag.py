ac9b9aab187cfe74178781677c40b79ecfab6350
15814 <acat99@foxmail.com>
2018-06-02 09:34:47 +0800

    put the description of problem to readme.md

--- sorts/Netherlands_flag.py
+++ sorts/Netherlands_flag.py
@@ -1,7 +1,4 @@
 
-# 问题二(荷兰国旗问题)
-# 给定一个数组arr，和一个数num，请把小于num的数放在数组的 左边，等于num的数放在数组的中间，大于num的数放在数组的 右边。
-# 要求额外空间复杂度O(1)，时间复杂度O(N)
 
 def netherlands_flag(elements: list, num: int):
     length = len(elements)
