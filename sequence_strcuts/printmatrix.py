92547cc470541f300de0751609adbcaf02430ca3
15814 <acat99@foxmail.com>
2018-06-03 16:18:43 +0800

    first solution

--- sequence_strcuts/printmatrix.py
+++ sequence_strcuts/printmatrix.py
@@ -8,3 +8,22 @@ def printrectangle(matrix, leftrow, leftcol, rightrow, rightcol):
             print(matrix[rightcol][i], end=' ')
         for i in range(rightrow, leftrow, -1):
             print(matrix[i][leftcol], end=' ')
+def printmatrix(matrix):
+    if matrix:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        leftrow = 0
+        leftcol = 0
+        rightrow = rows - 1
+        rightcol = cols - 1
+
+        while(leftrow <= rightrow):
+            printrectangle(matrix, leftrow, leftcol, rightrow, rightcol)
+            leftrow += 1
+            leftcol += 1
+            rightrow -= 1
+            rightcol -= 1
+
+        print('\n')
+    return
+
