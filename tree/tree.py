96c4f9d780c2f195c4394f6ef2cf99c90b56fc5e
15814 <acat99@foxmail.com>
2018-06-06 19:55:36 +0800

    add tree class basic

---
+++ tree/tree.py
@@ -0,0 +1,33 @@
+
+class TreeNode(object):
+    """docstring for Node."""
+
+    def __init__(self, data = None):
+        super(TreeNode, self).__init__()
+        self.data = data
+        self.lchild = None
+        self.rchild = None
+
+
+class Tree(object):
+    """docstring for Tree."""
+
+    def __init__(self, data = None):
+        super(Tree, self).__init__()
+        self.root = TreeNode(data)
+
+
+    def addchild(self, lchild = None, rchild = None):
+        
+        pass
+
+
+
+
+
+
+
+
+
+
+# ----
