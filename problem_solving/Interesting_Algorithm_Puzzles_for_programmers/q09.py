fcc7fc25eb7ace3958a1eb883740074269513aaa
15814 <acat99@foxmail.com>
2018-12-02 08:08:49 +0800

    first try in q09

---
+++ problem_solving/Interesting_Algorithm_Puzzles_for_programmers/q09.py
@@ -0,0 +1,21 @@
+
+# 20,10
+def q09(male_num=20, female_num=10):
+  men = [-1]*male_num
+  women = [1]*female_num
+
+  seq = [1]*(male_num+female_num)
+
+  for elem in seq:
+
+
+
+
+def get_seqs(seq,pos):
+
+
+
+
+
+if __name__ == '__main__':
+  q09()
